2a12c455172c0daf709b16949d445a1b86f04c22
texydo <roeeidan@post.bgu.ac.il>
2024-03-27 14:01:46 +0200

    nothing new

--- run_main_threads.sh
+++ run_main_threads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=threads_main
-#SBATCH --cpus-per-task=164
+#SBATCH --cpus-per-task=210
 #SBATCH --mem=150G
 #SBATCH --output=/sise/home/roeeidan/icarus_framework/main_logs/job_main_output-%j.txt
 
-/home/roeeidan/.conda/envs/icarus/bin/python /home/roeeidan/icarus_framework/main.py 0 164  /home/roeeidan/icarus_framework/outputs
+/home/roeeidan/.conda/envs/icarus/bin/python /home/roeeidan/icarus_framework/main.py 0 210  /home/roeeidan/icarus_framework/outputs
